2bf6bd8b85782d56c204f1fda6d96dcdd6f7e615
04tete <04tetec@gmail.com>
2026-06-19 16:31:37 -0400

    Primera versión del programa, funciones y estructura basica

---
+++ evainformatica_3.py
@@ -0,0 +1,94 @@
+#Sistema Rápido de Trazabilidad de Muestras de Laboratorio 
+
+muestras_lab=[]
+
+#Definiciones de opciones disponibles en el menu del sistema
+
+#Registro de muestra
+def registro_muestra():
+
+    num_muestra = str(input("Registre numero de muestra: ")).strip().upper()
+    nombre_paciente = str(input("Registre nombre del paciente: ")).upper()
+    temp_muestra = int(input("Registre temperatura de muestra (en C*): "))
+
+    #Logica de temperaturas
+
+    if temp_muestra < 2 or temp_muestra > 8:
+        print("Temperatura fuera de rango establecido por codigo")
+        print("Estado: RECHAZADO")
+        estado_muestra = "RECHAZADO"
+        
+
+    elif temp_muestra > 2 and temp_muestra < 8:
+        print("Muestra dentro de rango estandar.")
+        print("Estado: PENDIENTE")
+        estado_muestra = "PENDIENTE"
+
+    #Añadir a lista de muestras de laboratorio.
+    
+    idmuestra = [num_muestra, nombre_paciente, estado_muestra]
+    
+    muestras_lab.append(idmuestra)
+    return
+
+#Busqueda y gestion de muestras
+def gestion_muestras():
+
+    muestra_busqueda = input(str("Registre numero de muestra a buscar: ")).strip().upper()
+
+    for x in muestras_lab:
+        if len(muestras_lab) == 0:
+            print("No hay muestras en el sistema.")
+
+        else:
+            print (muestras_lab), "Se encuentra en el sistema, a continuacion vera sus opciones."
+
+            print("""
+            ---- Seccion de gestion ----
+                  
+             1. Modificar Estado de Muestra
+             2. Eliminar  Muestra
+
+                  """)
+            submuestra_input=int(input("Ingrese opcion: "))
+    
+            if submuestra_input == 1:
+
+                nuevo_estadomuestra = str(input("Ingrese nuevo estado: "))
+                muestras_lab.insert[2]
+
+            elif submuestra_input == 2:
+                muestras_lab.remove(muestra_busqueda)
+            else:
+                print("Opcion no valida.")
+    
+    return
+#Menu
+def menu_sistema():
+    
+    print("""
+          
+------------------ Sistema de Trazabilidad de Muestras de Laboratorio ------------------
+                     -- Sistema de Información del Laboratorio --
+
+            1. Registros de Muestras
+            2. Busqueda y gestion de Muestras
+            3. Salida
+          
+          """)
+
+#Programa
+while True:
+
+    menu_sistema()
+    
+    menu_input = input("Ingrese opcion: ").strip().upper()
+
+    if menu_input == "1":
+        registro_muestra()
+
+    elif menu_input == "2":
+        gestion_muestras()
+
+    elif menu_input == "3":
+        break
